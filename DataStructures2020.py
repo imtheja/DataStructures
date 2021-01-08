798252b62e2b98df83bbf6872570108b4f1a8b82
imtheja <imtheja@gmail.com>
2021-01-08 10:11:22 -0500

    updated Array with Pop method

--- DataStructures2020.py
+++ DataStructures2020.py
@@ -19,15 +19,44 @@ class TejasArray:
             for i in range (self.length, index-1, -1):
                 if i==index:
                     self.data[i]=strValue
+                    self.length+=1
                     print(self.data)
                     return
                 else:
                     self.data[i]=self.data[i-1]
             self.length+=1
-    def get(self):
-    def pop(self):
-    def remove(self):
-    def sort(self):
+    def get(self, index):
+        if index > self.length-1:
+            print ("Not a valid index ")
+            return
+        else:
+            print(self.data[index])
+
+    def pop(self, index="-1"):
+        if index==-1:
+            strPopItem=self.data[self.length-1]
+            self.lenth-=1
+            print(strPopItem)
+            return strPopItem
+        elif index > self.length-1:
+            print ("Not a valid index ")
+            return
+        else:
+            for i in range (index, self.length-1):
+                if i == index:
+                    strPopItem=self.data[i]
+                    self.length-=1
+                self.data[i]=self.data[i+1]
+            print(strPopItem)
+            return
+
+
+
+
+
+
+    #def remove(self):
+    #def sort(self):
 
 
 SampleArray=TejasArray()
@@ -35,3 +64,5 @@ SampleArray.append(10)
 SampleArray.append(30)
 SampleArray.append(40)
 SampleArray.insert(0, 20)
+SampleArray.get(3)
+SampleArray.pop(10)
