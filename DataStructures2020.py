793b84dbb7476a11926da50adea2d310850530d8
imtheja <imtheja@gmail.com>
2021-01-14 15:02:13 -0500

    Arrays Solutions in MustDoQuestions

--- DataStructures2020.py
+++ DataStructures2020.py
@@ -9,9 +9,9 @@ class TejasArray:
         self.length+=1
         print(self.data)
     def insert(self, index, value):
-        if self.length==0 or index==self.length + 1 :
+        if self.length==0 or index==self.length :
             TejasArray.append(self,value)
-        elif index > self.length+1:
+        elif index >= self.length+1:
             print("Not a valid index")
             return
         else:
@@ -24,7 +24,9 @@ class TejasArray:
                     return
                 else:
                     self.data[i]=self.data[i-1]
-            self.length+=1
+        print(self.length)
+        print(self.data)
+
     def get(self, index):
         if index > self.length-1:
             print ("Not a valid index ")
@@ -33,10 +35,13 @@ class TejasArray:
             print(self.data[index])
 
     def pop(self, index="-1"):
-        if index==-1:
+        if index=="-1" or index == self.length-1:
             strPopItem=self.data[self.length-1]
-            self.lenth-=1
+            del self.data[self.length-1]
+            self.length-=1
             print(strPopItem)
+            print(self.data)
+            print(self.length)
             return strPopItem
         elif index > self.length-1:
             print ("Not a valid index ")
@@ -45,24 +50,36 @@ class TejasArray:
             for i in range (index, self.length-1):
                 if i == index:
                     strPopItem=self.data[i]
-                    self.length-=1
                 self.data[i]=self.data[i+1]
+            del self.data[self.length-1]
+            self.length-=1
             print(strPopItem)
+            print(self.data)
+            print(self.length)
             return
 
-
-
-
-
-
-    #def remove(self):
-    #def sort(self):
+    def sort(self):
+        counter=0
+        while counter < self.length:
+            for i in range(self.length-1):
+                if self.data[i]>self.data[i+1]:
+                    self.data[i+1], self.data[i] = self.data[i], self.data[i+1]
+            counter+=1
+        print(self.data)
+        return self.data
 
 
 SampleArray=TejasArray()
+SampleArray.append(80)
 SampleArray.append(10)
-SampleArray.append(30)
-SampleArray.append(40)
-SampleArray.insert(0, 20)
-SampleArray.get(3)
-SampleArray.pop(10)
+SampleArray.append(1000)
+SampleArray.insert(3, 2000)
+#SampleArray.insert(0, 'a')
+#SampleArray.insert(2, 'b')
+#SampleArray.insert(4, 'c')
+
+#SampleArray.get(3)
+#SampleArray.pop()
+#SampleArray.pop(3)
+#SampleArray.pop(2)
+SampleArray.sort()
---
+++ MustDoQuestions.py
@@ -0,0 +1,110 @@
+#Leet Code Questions
+#Arrays: Move Zeros. nums=[1,2,0,3]--> [1,2,3,0]
+class Questions:
+    def MoveZeros(self, nums):
+        #Brute Force
+        intCounter=0
+        while intCounter < len(nums):
+            for i in range(len(nums)-1):
+                if nums[i] != nums[i+1] and nums[i]==0:
+                    nums[i], nums[i+1]= nums[i+1], nums[i]
+            intCounter+=1
+        return print(nums)
+
+    def MoveZeros2(self, nums):
+        #Questions
+        #Break it down to smallest pieces of work
+        #Approach
+        #0(n)
+        #Space Complexity
+        #Edge Cases
+        #Where to improve
+        intCounter=0
+        for indx in range(len(nums)):
+            if nums[indx]!=0:
+                nums[intCounter]=nums[indx]
+                intCounter+=1
+        for paddingIndx in range(intCounter,len(nums)):
+            nums[paddingIndx]=0
+        print(nums)
+        return nums
+
+    def TwoSum(self, nums, target):
+        dictStaging={}
+        for indx, number in enumerate(nums):
+            if target-number in dictStaging.keys():
+                print([indx, dictStaging[target-number]])
+                return [indx, dictStaging[target-number]]
+            else:
+                dictStaging[number] = indx
+        return False
+
+    def NumRescueBoats(self, weights, limit):
+        #Questions: Weights are not going to be <=0
+        weights.sort()
+        print(weights)
+        intBoatsRequired=0
+        intLowerIndex=0
+        intUpperIndex=len(weights)-1
+        while intLowerIndex<=intUpperIndex:
+            if intLowerIndex==intUpperIndex:
+                intBoatsRequired+=1
+                break
+            if weights[intLowerIndex]+weights[intUpperIndex] <=limit:
+                intBoatsRequired+=1
+                intLowerIndex+=1
+                intUpperIndex-=1
+            else:
+                intBoatsRequired+=1
+                intUpperIndex-=1
+
+        print(intBoatsRequired)
+        return intBoatsRequired
+    def ValidMountainArray(self, arr):
+        #Questions
+        #Break it down to smallest pieces of work
+        #Approach: Two Forloops with a counter and validating counter
+        #0(n)
+        #Space Complexity
+        #Edge Cases
+        #Where to improve: where to start the counter number and what will it be in the end.
+        if len(arr)<3:
+            return False
+        intCounter=1
+        while intCounter < len(arr) and arr[intCounter]> arr[intCounter-1]:
+            intCounter+=1
+        if intCounter==1 or intCounter==len(arr)-1:
+            return False
+        while intCounter < len(arr) and arr[intCounter]< arr[intCounter-1]:
+            intCounter+=1
+        return print(intCounter==len(arr))
+
+    def ContainerWithMostWater(self, nums):
+        #Approach: 2 Pointers Approach
+        #edgecase: when n is 0 (i misesed it)
+        if len(nums)==0:
+            return 0
+
+        intLowerIndex=0
+        intUpperIndex=len(nums)-1
+        intMaxWater=0
+        intArea=0
+        while intLowerIndex < intUpperIndex:
+            intArea=(intUpperIndex-intLowerIndex)*min(nums[intLowerIndex], nums[intUpperIndex])
+            intMaxWater = max(intMaxWater, intArea)
+            if nums[intLowerIndex]<=nums[intUpperIndex]:
+                intLowerIndex+=1
+            else:
+                intUpperIndex-=1
+        return print(intMaxWater)
+
+
+
+TestInstance=Questions()
+#TestInstance.MoveZeros2([1,0,2,0,3,0,4,0,5,0,0,0,7,8,9])
+#TestInstance.NumRescueBoats([1,2], 3)
+#TestInstance.ValidMountainArray([0,3,2,1])
+TestInstance.ContainerWithMostWater([4,3,2,1,4])
+TestInstance.ContainerWithMostWater([1,1])
+TestInstance.ContainerWithMostWater([1,2,1])
+#TestInstance.TwoSum([7,2,11,15], 9 )
