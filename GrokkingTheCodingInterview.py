bae45374fa3da0864b3a1943f7d75de1eeaf02e4
imtheja <imtheja@gmail.com>
2021-03-02 12:59:16 -0500

    march 2 sliding window problems

--- GrokkingTheCodingInterview.py
+++ GrokkingTheCodingInterview.py
@@ -1,6 +1,6 @@
 #Grokking the coding interview
 #Chapter 1. Sliding Window
-
+import math
 class slidingWindow:
     def maximumSumSubArray(self, nums, k):
         intWindowSum= sum(nums[i] for i in range(k))
@@ -12,6 +12,38 @@ class slidingWindow:
             intMaxSum = max(intWindowSum, intMaxSum)
         return intMaxSum
 
+    def minimumSubArray(self, nums, k):
+        intWindowSum=0
+        intStartIndex=0
+        intMinSubArrayLength= math.inf
+        intSubArrayLength=0
+        intArrayLength=len(nums)
+        for i in range(intArrayLength):
+            intWindowSum+=nums[i]
+            while intWindowSum >= k:
+                intSubArrayLength = i - intStartIndex +1
+                intMinSubArrayLength = min(intMinSubArrayLength, intSubArrayLength)
+                intWindowSum -= nums[intStartIndex]
+                intStartIndex += 1
+        if intMinSubArrayLength == math.inf:
+            return 0
+        else:
+            return intMinSubArrayLength
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 TestInstance=slidingWindow()
 print(TestInstance.maximumSumSubArray([2,1,5,1,3,2], 3))
+print(TestInstance.minimumSubArray([2, 1, 5, 2, 3, 2], 7))
 
