d3cf6f83c81172ea55e362a83656fa3ae01945b8
imtheja <imtheja@gmail.com>
2021-04-20 02:59:01 -0400

    Day3

--- 100DaysChallenge.py
+++ 100DaysChallenge.py
@@ -88,9 +88,38 @@ class HundredDaysChallenge:
             intLongestSubstring =max(intLongestSubstring, sum(dictStaging.values()))
         return intLongestSubstring
 
+    '''
+    Given an array of characters where each character represents a fruit tree, 
+    you are given two baskets and your goal is to put maximum number of fruits in each basket. 
+    The only restriction is that each basket can have only one type of fruit.
+    You can start with any tree, but once you have started you can’t skip a tree. 
+    You will pick one fruit from each tree until you cannot, i.e., you will stop when 
+    you have to pick from a third fruit type.
+    Write a function to return the maximum number of fruits in both the baskets.
+    '''
+
+    def maximumFruitsInBaskets(self, trees):
+        dictStaging={}
+        intStartIndex=0
+        intMaxFruits=0
+        for intEndIndex in range(len(trees)):
+            if trees[intEndIndex] in dictStaging:
+                dictStaging[trees[intEndIndex]]+=1
+            else:
+                dictStaging[trees[intEndIndex]]=1
+
+            while len(dictStaging) > 2:
+                if dictStaging[trees[intStartIndex]]==1:
+                    del dictStaging[trees[intStartIndex]]
+                else:
+                    dictStaging[trees[intStartIndex]]-=1
+                intStartIndex+=1
+            intMaxFruits = max(intMaxFruits, sum(dictStaging.values()))
+        return intMaxFruits
 
 resultObject=HundredDaysChallenge()
 #print(resultObject.maximumSubarraySumOfK([2, 3, 4, 1, 5], 2))
 #print(resultObject.smallestSubarrarSumGreaterThank([3, 4, 1, 1, 6], 8))
 #print(resultObject.longestSubstringWithKDistinctChars("araaci", 1))
-print(resultObject.longestSubstringAfterReplacingKLetters("abccde", 1))
+#print(resultObject.longestSubstringAfterReplacingKLetters("abccde", 1))
+#print(resultObject.maximumFruitsInBaskets(['A', 'B', 'C', 'B', 'B', 'C']))
